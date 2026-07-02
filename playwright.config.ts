d05fef6d665da7551e9117be240c33006499abd9
Inna Logvin <innanlogvin@gmail.com>
2026-07-02 22:17:21 +0300

    Playwright locators, actions and assertions

--- playwright.config.ts
+++ playwright.config.ts
@@ -27,12 +27,16 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
-    // baseURL: 'http://localhost:3000',
+    baseURL: 'https://qauto.forstudy.space/',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
     video: 'on',
-    screenshot: 'on'
+    screenshot: 'on',
+    httpCredentials:{
+      username: 'guest',
+      password: 'welcome2qauto'
+    }
   },
 
   /* Configure projects for major browsers */
